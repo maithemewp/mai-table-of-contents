0c181389b0e9b6253c37d6942017d53b6e73db15
Mike Hemberger <mike@thestizmedia.com>
2021-09-15 15:27:54 -0400

    fix custom block settings not working

--- includes/class-table-of-contents.php
+++ includes/class-table-of-contents.php
@@ -48,7 +48,7 @@ class Mai_Table_Of_Contents {
 	}
 
 	function do_toc( $block, $content = '', $is_preview = false ) {
-		$custom = get_field( 'mai-toc_custom' );
+		$custom = get_field( 'maitoc_custom' );
 		$args   = [
 			'open'     => $custom ? get_field( 'maitoc_open' ) : get_option( 'options_maitoc_open', true ),
 			'headings' => $custom ? get_field( 'maitoc_headings' ) : get_option( 'options_maitoc_headings', 2 ),
