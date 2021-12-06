edcc6230f380e831da1d7ccaf53a841f5dc8a592
Mike Hemberger <mike@thestizmedia.com>
2021-12-06 12:33:28 -0500

    Move settings page under Mai Theme

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -1,43 +1,57 @@
 <?php
 
+// Prevent direct file access.
+defined( 'ABSPATH' ) || die;
+
 // Get it started.
 add_action( 'acf/init', function() {
+	if ( ! function_exists( 'acf_add_options_page' ) ) {
+		return;
+	}
+
 	new Mai_Table_Of_Contents_Settings;
 });
 
 class Mai_Table_Of_Contents_Settings {
 
 	function __construct() {
-
-		// Bail if ACF is not active.
-		if ( ! function_exists( 'acf_add_options_page' ) ) {
-			return;
-		}
-
-		// Hooks.
 		$this->hooks();
-
 	}
 
+	/**
+	 * Runs hooks.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return void
+	 */
 	function hooks() {
+		acf_add_options_sub_page(
+			[
+				'page_title' => __( 'Mai Table of Contents', 'mai-table-of-contents' ),
+				'menu_title' => __( 'Table of Contents', 'mai-table-of-contents' ),
+				'menu_slug'  => 'mai-table-of-contents',
+				'parent'     => class_exists( 'Mai_Engine' ) ? 'mai-theme' : 'options-general.php',
+				'capability' => 'manage_options',
+				'position'   => 4,
+				'redirect'   => false
+			]
+		);
 
-		// Add the options pages.
-		acf_add_options_page( array(
-			'page_title' => __( 'Mai Table of Contents', 'mai-table-of-contents' ),
-			'menu_title' => __( 'Table of Contents', 'mai-table-of-contents' ),
-			'menu_slug'  => 'mai-table-of-contents',
-			'parent'     => 'options-general.php',
-			'capability' => 'manage_options',
-			'redirect'   => false
-		) );
-
-		// Add custom CSS.
-		add_action( 'acf/input/admin_head', array( $this, 'custom_css' ) );
-
-		add_filter( 'acf/load_field/key=field_5dd59edcd62e7', array( $this, 'get_post_types' ) );
+		add_action( 'acf/render_field/key=field_5dd59edcd62e7', [ $this, 'custom_css' ] );
+		add_filter( 'acf/load_field/key=field_5dd59edcd62e7',   [ $this, 'get_post_types' ] );
 	}
 
-	function custom_css() {
+	/**
+	 * Adds custom CSS in the first field.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param array The field data.
+	 *
+	 * @return void
+	 */
+	function custom_css( $field ) {
 		?>
 		<style>
 			input#acf-field_5dc5ab7ea6e00 {
@@ -47,6 +61,15 @@ class Mai_Table_Of_Contents_Settings {
 		<?php
 	}
 
+	/**
+	 * Gets post types for settings.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param array The field data.
+	 *
+	 * @return array
+	 */
 	function get_post_types( $field ) {
 
 		// Reset choices.
