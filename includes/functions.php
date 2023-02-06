a88442e9846cbf4ce15b8b949b7e598e7532aca5
JiveDig <jivetheuniversal@gmail.com>
2023-02-06 16:11:19 -0500

    Ignore nesting when checking for h2/h3

--- includes/functions.php
+++ includes/functions.php
@@ -39,70 +39,88 @@ function maitoc_get_data( $content ) {
 	// Restore.
 	libxml_use_internal_errors( $libxml_previous_state );
 
-	// h2s.
-	$h2s = $dom->getElementsByTagName( 'h2' );
+	$xpath = new DOMXPath( $dom );
+	$h2h3  = $xpath->query( '//h2 | //h3' );
 
-	// Bail less than 2 h2s.
-	if ( ! $h2s || ( $h2s->length < 2 ) ) {
+	// Bail if no headings.
+	if ( ! $h2h3 ) {
 		return $data;
 	}
 
-	$xpath = new DOMXPath( $dom );
-	$reset = true;
+	$reset         = true;
+	$current_h2_id = false;
 
-	// Loop through h2s.
-	foreach ( $h2s as $index => $h2 ) {
-		$text  = $h2->nodeValue;
-		$id    = $h2->getAttribute( 'id' );
-
-		if ( ! $id ) {
-			$id = sanitize_title( $text );
+	foreach ( $h2h3 as $index => $node ) {
+		// Skip if another heading is before the first h2.
+		if ( ! $current_h2_id && 'h2' !== $node->nodeName ) {
+			continue;
 		}
 
-		$id    = maitoc_get_unique_id( $id, $reset );
+		// Set vars.
+		$id    = maitoc_get_node_id( $node, $reset );
+		$text  = $node->nodeValue;
 		$reset = false;
-		$h2->setAttribute( 'id', $id );
 
-		$data['matches'][ $index ] = [
-			'id'       => $id,
-			'text'     => $text,
-			'children' => [],
-		];
+		// Set id.
+		$node->setAttribute( 'id', $id );
 
-		$h3s      = [];
-		$h3_reset = true;
+		// Build data.
+		switch ( $node->nodeName ) {
+			case 'h2':
+				// Set current.
+				$current_h2_id = $id;
 
-		// Loop through next sibling elements, and stop at the next h2.
-		while( ( $h2 = $h2->nextSibling ) && ( 'h2' !== $h2->nodeName ) ) {
-			$h3s = $xpath->query( 'descendant-or-self::h3', $h2 );
-
-			if ( ! $h3s->length ) {
-				continue;
-			}
-
-			foreach ( $h3s as $h3 ) {
-				$text  = $h3->nodeValue;
-				$id    = $h3->getAttribute( 'id' );
-
-				if ( ! $id ) {
-					$id = sanitize_title( $text );
-				}
-
-				$id       = maitoc_get_unique_id( $id, $h3_reset );
-				$h3_reset = false;
-				$h3->setAttribute( 'id', $id );
-
-				$data['matches'][ $index ]['children'][] = [
+				// Add to data.
+				$data['matches'][ $current_h2_id ] = [
+					'id'       => $id,
+					'text'     => $text,
+					'children' => [],
+				];
+			break;
+			case 'h3':
+				// Add to data.
+				$data['matches'][ $current_h2_id ]['children'][] = [
 					'id'   => $id,
 					'text' => $text,
 				];
-			}
+			break;
 		}
 	}
 
-	$data['content'] = $dom->saveHTML( $dom->documentElement );
+	// If we have at least 3 h2 headings.
+	if ( count( $data['matches'] ) > 2 ) {
+		// Store TOC in new content.
+		$data['content'] = $dom->saveHTML( $dom->documentElement );
+	}
+	// Not enough headings.
+	else {
+		// Clear matches.
+		$data['matches'] = [];
+	}
 
 	return $data;
+}
+
+/**
+ * Gets a unique id value from node.
+ * See `maitoc_get_unique_id()` for explanation.
+ *
+ * @since TBD
+ *
+ * @param string $id    The existing id to check.
+ * @param bool   $reset Reset the anchors.
+ *
+ * @return string
+ */
+function maitoc_get_node_id( $node, $reset = false ) {
+	$text  = $node->nodeValue;
+	$id    = $node->getAttribute( 'id' );
+
+	if ( ! $id && ! empty( $node->nodeValue ) ) {
+		$id = sanitize_title( $node->nodeValue );
+	}
+
+	return maitoc_get_unique_id( $id, $reset );
 }
 
 /**
@@ -110,7 +128,7 @@ function maitoc_get_data( $content ) {
  * Each time an id is found the total number is incremented
  * as the anchor value.
  *
- * Reset parameters is needed because the data is created once for the toc
+ * Reset parameter is needed because the data is created once for the toc
  * and once in the actual content so without it the heading id's are incremented
  * continuously and won't match.
  *
@@ -122,7 +140,7 @@ function maitoc_get_data( $content ) {
  * @since 1.4.0
  *
  * @param string $id    The existing id to check.
- * @param bool   $reset Reset the ancrhos.
+ * @param bool   $reset Reset the anchors.
  *
  * @return string
  */
