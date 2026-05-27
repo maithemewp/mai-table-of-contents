b115ac0c9e93eb9f8149c7595328c24cfed0fb53
JiveDig <540990+JiveDig@users.noreply.github.com>
2026-05-27 12:52:29 -0400

    1.6.7 update the updater for PHP 8.4 support

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'f6d4f6bcee7247df6b777884c3e22f98' => $vendorDir . '/yahnis-elsts/plugin-update-checker/load-v5p6.php',
+    'bc0af1337b39f0d750e835f5263eb646' => $vendorDir . '/yahnis-elsts/plugin-update-checker/load-v5p7.php',
 );
